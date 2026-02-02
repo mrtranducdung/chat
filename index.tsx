21a0ba892fa04157c024a5a04bea2a883d633f28
mrtranducdung <mrtranducdung@gmail.com>
2026-02-02 12:09:14 +0900

    Add Widget route to index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import Widget from './Widget';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -10,6 +12,11 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/widget" element={<Widget />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
